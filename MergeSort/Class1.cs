782c09ab8e23e71131cef5f717b525dbaca00ba9
Mathew Olsen <mathew.olsen@snow.edu>
2024-09-11 14:11:37 -0600

    Wrote intial code to combine two sorted arrays into a final sorted array.

---
+++ MergeSort/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace MergeSort;
+
+public class Lab2
+{
+
+    
+
+    public static int[] CombineSortedArrays(int[] a, int[] b)
+    {
+        
+        int[] c = new int[a.Length + b.Length];
+
+        int count1 = 0;
+        int count2 = 0;
+       
+        for (int i = 0; i <= c.Length; i++)
+        {
+            if (a[count1] <= b[count2])
+            {
+                c[count1 + count2] = a[count1++];
+            }
+            else
+            {
+                c[count1 + count2] = b[count2++];
+            }
+        }
+        return c;
+    }
+
+
+}
---
+++ Testing/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Testing;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Debug.Assert(Lab2.CombineSortedArrays([1,2,4,6,8], [3,6,8,10,14]) == [1,2,3,4,6,6,7,7,10,14], "True");
+    }
+}
