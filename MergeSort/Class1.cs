c036fc031ed9345482615c0a91d7ddcf065d1916
Mathew Olsen <mathew.olsen@snow.edu>
2024-09-11 16:31:05 -0600

    Added all comments

--- MergeSort/Class1.cs
+++ MergeSort/Class1.cs
@@ -4,13 +4,15 @@ namespace MergeSort;
 
 public static class Lab2{
 
+//Method to split and sort using CombineSortedArrays method.
 public static int[] SortViaMergeSort(int[] values)
 {
+    //splits the array into two halves
     int middle = values.Length / 2;
-
     int[] firstHalf = values[0..middle];
     int[] secondHalf = values[middle..values.Length];
 
+    //checks if the arrays are the correct length, if they are not, the method runs again via recursion.
     if (values.Length < 2){
         return values;
     }
@@ -19,15 +21,20 @@ public static int[] SortViaMergeSort(int[] values)
     }
 }
 
+//Method used to combine two already sorted arrays into a larger sorted array.
 public static int[] CombineSortedArrays(int[] a, int[] b)
     {
+        //creates the larger array using the length of the two arrays.
         int[] c = new int[a.Length + b.Length];
 
+        //initializes a count for each array to track how to combine.
         int count1 = 0;
         int count2 = 0;
        
+        //loops to create the larger array from the two smaller.
         while(count1 + count2 < c.Length)
         {
+            //checks to see if either array has been fully used, if not then it sorts and adds to the array until one is complete.
             if (count1 == a.Length){
                 c[count1 + count2] = b[count2++];
             }
