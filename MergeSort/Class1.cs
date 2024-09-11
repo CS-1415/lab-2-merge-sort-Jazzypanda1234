2f6976262da148939265ce1bae5238367db8307e
Mathew Olsen <mathew.olsen@snow.edu>
2024-09-11 16:13:16 -0600

    Test 1

--- MergeSort/Class1.cs
+++ MergeSort/Class1.cs
@@ -1,31 +1,46 @@
-﻿namespace MergeSort;
+﻿// Mathew Olsen 9/11/24 Lab 2 Merge Sort
 
-public class Lab2
+namespace MergeSort;
+
+public static class Lab2{
+
+public static int[] SortViaMergeSort(int[] values)
 {
+    int middle = values.Length / 2;
 
-    
+    int[] firstHalf = values[0..middle];
+    int[] secondHalf = values[middle..values.Length];
 
-    public static int[] CombineSortedArrays(int[] a, int[] b)
+    if (values.Length < 2){
+        return values;
+    }
+    else{
+        return CombineSortedArrays(SortViaMergeSort(firstHalf), SortViaMergeSort(secondHalf));
+    }
+}
+
+public static int[] CombineSortedArrays(int[] a, int[] b)
     {
-        
         int[] c = new int[a.Length + b.Length];
 
         int count1 = 0;
         int count2 = 0;
        
-        for (int i = 0; i <= c.Length; i++)
+        while(count1 + count2 < c.Length)
         {
-            if (a[count1] <= b[count2])
-            {
+            if (count1 == a.Length){
+                c[count1 + count2] = b[count2++];
+            }
+            else if(count2 == b.Length){
                 c[count1 + count2] = a[count1++];
             }
-            else
-            {
+            else if (a[count1] <= b[count2]){
+                c[count1 + count2] = a[count1++];
+            }
+            else{
                 c[count1 + count2] = b[count2++];
             }
         }
         return c;
     }
-
-
 }
--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -1,10 +1,14 @@
 namespace Testing;
-
+using MergeSort;
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-        // Debug.Assert(Lab2.CombineSortedArrays([1,2,4,6,8], [3,6,8,10,14]) == [1,2,3,4,6,6,7,7,10,14], "True");
+        System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(
+            Lab2.CombineSortedArrays(new int[]{1, 3, 5}, new int[]{-5, 3, 6, 7}),
+            new int[]{-5, 1, 3, 3, 5, 6, 7}));
+
+       
     }
 }
